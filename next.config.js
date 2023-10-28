759256e2d6a639c7269d543a075a54f0b845d037
Lateef Aminu <122064166+reallife01@users.noreply.github.com>
2023-10-29 00:34:15 +0100

    config commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com",'encrypted-tbn0.gstatic.com'],
+    domains: ["images.pexels.com","encrypted-tbn0.gstatic.com"],
   },
 };
 
