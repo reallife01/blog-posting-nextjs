7b73fd2245d0224784023859d38d4d337832da9b
Lateef Aminu <122064166+reallife01@users.noreply.github.com>
2023-10-29 00:18:43 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.pexels.com",'encrypted-tbn0.gstatic.com'],
+  },
+};
+
+module.exports = nextConfig;
